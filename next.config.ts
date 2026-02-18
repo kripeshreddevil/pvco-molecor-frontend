8dddcdef053699fc37f26457394a13bc0fec1796
kripeshlamichhane <kripeshlamichhane@gmail.com>
2026-02-18 20:30:07 +0545

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,34 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/**', // All local images
+      },
+      {
+        protocol: 'https',
+        hostname: 'pvco-molecor-backend.onrender.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**', // Only if you use this host
+      },
+    ],
+  },
+};
+
+export default nextConfig;
